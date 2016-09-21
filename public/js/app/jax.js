b0bf97dcd8a1dce79060245211d00758c7e93d1e
Martin Keefe <martin@martinkeefe.com>
2016-09-21 18:33:08 +0100

    Moved to simple SPA using location.hash.

---
+++ public/js/app/jax.js
@@ -0,0 +1,44 @@
+define(function(require, exports, module) {
+    var $ = require('zepto');
+    var _ = require('lodash');
+
+    function syms(i, elem) {
+        var text = elem.innerHTML;
+        var html = '<table class="centre">';
+        _.each(text.split('\n'), function (line) {
+            html += '<tr>';
+            _.each(line.split(' '), function (code) {
+                if (code === '_') {
+                    html += '<td></td><td></td>';
+                } else if (code !== '') {
+                    html += '<td class="sym">\\('+code+'\\)</td><td class="code"><pre><code>'+code+'</code></pre></td>';
+                }
+            });
+            html += '</tr>';
+        });
+        html += '</table>';
+        elem.innerHTML = html;
+    }
+
+    MathJax.Hub.Config({
+      extensions: ["tex2jax.js"],
+      "HTML-CSS": { scale: 100, availableFonts: [], webFont: "TeX"}
+    });
+
+    return function(num, render) {
+        render('jax-text', 'jax/jax' + num, function() {
+            $('div.syms').each(syms);
+            $('tex').each(function (i, elem) {
+                $(elem).replaceWith('<pre class="code"><code class="tex">'+elem.innerHTML+'</code></pre>');
+            });
+
+            MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+
+            MathJax.Hub.Queue(function() {
+                hljs.tabReplace = '    ';
+                hljs.initHighlighting.called = false;
+                hljs.initHighlighting();
+            });
+        });
+    }
+});
--- public/js/index.js
+++ public/js/index.js
@@ -2,70 +2,66 @@
 // =============================================================================
 // Derived from https://github.com/miazukee/restlite
 
-var restlite = {};
+var routes = [];
+var error404 = '#404';
 
-restlite.routes = [];
-restlite.error404 = '#404';
-
-restlite.route = function() {
-  //var r = window.location.pathname;
-  var r = window.location.hash.substr(1).split('?');
-  var a = r[1];
-  r = r[0];
-  if(restlite.routes[r] !== undefined)
-    restlite.routes[r](a);
-  else if(restlite.routes[restlite.error404] !== undefined)
-    restlite.routes[restlite.error404](a);
+function route() {
+	var r = window.location.hash.substr(1).split('?');
+	var a = r[1];
+	r = r[0];
+	if(routes[r] !== undefined)
+		routes[r](a);
+	else if(routes[error404] !== undefined)
+		routes[error404](a);
 };
 
-restlite.render = function(id, path, success) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (xhttp.readyState === 4 && xhttp.status === 200) {
-      document.getElementById(id).innerHTML = xhttp.responseText;
-      if (success) {
-        success();
-      }
-    }
-  };
-  xhttp.open('GET', path, true);
-  xhttp.send();
+function render(id, path, success) {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (xhttp.readyState === 4 && xhttp.status === 200) {
+			document.getElementById(id).innerHTML = xhttp.responseText;
+			if (success) {
+				success();
+			}
+		}
+	};
+	xhttp.open('GET', '/html/' + path + '.html', true);
+	xhttp.send();
 };
 
-//restlite.setRoute = function(r) {
-//  history.pushState({}, '', r);
-//  restlite.route();
-//};
-//
-//window.addEventListener('popstate', function() {
-//  restlite.route();
-//});
 window.addEventListener('hashchange', function() {
-  restlite.route();
+	route();
 });
 
 // =============================================================================
 
-restlite.routes[''] = function() {
-  restlite.render('content', '/partials/front.html', function() {
-    requirejs(['app/shadebob'], function(sb) {
-      sb.resize();
-    });
-  })
+routes[''] = function() {
+	render('content', 'front', function() {
+		requirejs(['app/shadebob'], function(sb) {
+			sb.resize();
+		});
+	})
 };
-//restlite.routes['#'] = restlite.routes[''];
 
-restlite.routes['maths'] = function() {
-  restlite.render('content', '/partials/maths.html');
+routes['maths'] = function() {
+	render('content', 'maths');
 }
 
-restlite.routes['music/mnm'] = function(num) {
-  restlite.render('content', '/partials/mnm.html', function() {
-    requirejs(['app/mnm'], function(mnm) {
-      mnm(num);
-    });
-  });
+routes['music/mnm'] = function(num) {
+	render('content', 'mnm', function() {
+		requirejs(['app/mnm'], function(mnm) {
+			mnm(num);
+		});
+	});
 }
 
-restlite.route();
+routes['maths/jax'] = function(num) {
+	render('content', 'jax', function() {
+		requirejs(['app/jax'], function(jax) {
+			jax(num, render);
+		});
+	});
+}
+
+route();
 
