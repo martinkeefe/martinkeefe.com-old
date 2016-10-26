f3c839c115c7d88fe5bd4cb19c8e428f5f0b4e1b
Martin Keefe <budha57@hotmail.com>
2016-10-26 18:11:36 +0100

    Added HTML Boilerplate stuff.

--- serve.py
+++ serve.py
@@ -39,7 +39,7 @@ class S(BaseHTTPRequestHandler):
             f.close()
 
 def run(server_class=HTTPServer, handler_class=S, port=80):
-    server_address = ('', port)
+    server_address = ('0.0.0.0', port)
     httpd = server_class(server_address, handler_class)
     print 'Starting httpd...'
     httpd.serve_forever()
